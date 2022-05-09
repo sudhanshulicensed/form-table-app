377618ca98ca670473bf2b4f06b0acd7fd6a23b4
sudhanshulicensed <sudhanshu.licensed@gmail.com>
2022-05-09 18:24:21 +0530

    Added Basic Component to be used

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AddDetail />
+    <EditDetail />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import AddDetail from "./components/AddDetails.vue"
+import EditDetail from "./components/EditDetails.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    AddDetail,
+    EditDetail,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
